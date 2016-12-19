6f68a6b9b8b2aa3dca333e1c7efed370be964ca5
Jacklyne Bouier <jacklynebouier@Jacklyne-Bouiers-MacBook-Pro.local>
2016-12-19 09:43:05 -0800

    AddingJacky's promblem

--- Intermediate_Algorithms/1.Sum-All-Numbers-in-a-Range.js
+++ Intermediate_Algorithms/1.Sum-All-Numbers-in-a-Range.js
@@ -10,3 +10,19 @@ function sumAll(arr) {
 
 
 sumAll([5, 10]);
+
+/Jacky
+function sumAll(arr) {
+
+
+    var max = Math.max(arr[0], arr[1]);
+    var min = Math.min(arr[0], arr[1]);
+    var sum = 0;
+    for (var i=min; i <= max; i++){
+        sum += i;
+
+    }
+    return(sum);
+}
+
+sumAll([1, 4]);
